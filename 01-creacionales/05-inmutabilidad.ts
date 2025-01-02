47107428a229dea12c02f1ca05b5e8a42cfd6f2b
Faber Grajales <fabergrajales1999@gmail.com>
2025-01-02 01:35:28 -0500

    feat: Se suben cambios del patró prototype y el de inmutabilidad con copia

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -6,3 +6,101 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+import { COLORS } from '../helpers/colors.ts'
+
+class CodeEditorState {
+  readonly content: string
+  readonly cursorPosition: number
+  readonly unsavedChanges: boolean
+
+  constructor(content: string, cursorPosition: number, unsavedChanges: boolean) {
+    this.content = content
+    this.cursorPosition = cursorPosition
+    this.unsavedChanges = unsavedChanges
+  }
+
+  copyWith({ content, cursorPosition, unsavedChanges }: Partial<CodeEditorState>): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsavedChanges ?? this.unsavedChanges
+    )
+  }
+
+  displayState() {
+    console.log(`\n%cEditor State:`, COLORS.green)
+    console.log(`
+      Content: ${this.content}
+      Cursor Position: ${this.cursorPosition}
+      Unsaved Changes: ${this.unsavedChanges}
+      `)
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = []
+  private currentIndex: number = -1
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.slice(0, this.currentIndex + 1)
+    }
+
+    this.history.push(state)
+    this.currentIndex++
+  }
+
+  undo(): CodeEditorState | null {
+    if (this.currentIndex > 0) {
+      this.currentIndex--
+      return this.history[this.currentIndex]
+    }
+
+    return null
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++
+      return this.history[this.currentIndex]
+    }
+
+    return null
+  }
+}
+
+function main() {
+  const history = new CodeEditorHistory()
+  let editorState = new CodeEditorState('console.log("hola mundo");', 2, false)
+
+  console.log('%cEstado inicial:', COLORS.blue)
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log('%cSegundo cambio:', COLORS.blue)
+  editorState = editorState.copyWith({
+    content: 'console.log("hola mundo");\nconsole.log("adiós mundo");',
+    cursorPosition: 3,
+    unsavedChanges: true
+  })
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log('%cTercer cambio:', COLORS.blue)
+  editorState = editorState.copyWith({
+    cursorPosition: 5
+  })
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log('%cDeshacer:', COLORS.blue)
+  editorState = history.undo()!
+  editorState.displayState()
+
+  console.log('%cRehacer:', COLORS.blue)
+  editorState = history.redo()!
+  editorState.displayState()
+}
+
+main()
